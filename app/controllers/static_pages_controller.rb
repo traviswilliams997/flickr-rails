7ae66bb9bc3b425464229bf6e7d276c389daddb1
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-08-04 10:47:22 -0500

    Fix bug where response is nil before api call

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,25 @@
 class StaticPagesController < ApplicationController
 
-  #before_action :set_user_photos, only: [:index]
   def index
 
     require 'rest-client'
     require 'json'
 
-
-    url = 'https://www.flickr.com/services/rest/?'
-
-   
-
-    @response = RestClient.get( "#{url}method=flickr.people.getPublicPhotos&api_key=#{ENV['FLICKR_KEY']}&user_id=#{params[:flickr_id]}&format=json&nojsoncallback=1")
-    @user_photos = JSON.parse(@response)
+    url_base = 'https://www.flickr.com/services/rest/?'
 
    
-    @user_photos["photos"]["photo"].each do |photo| 
-
-      Url.new(photo["server"],photo["id"], photo["secret"])
-      
-      end 
   
+    
+    if params[:flickr_id].present?
+      @response = RestClient.get( "#{url_base}method=flickr.people.getPublicPhotos&api_key=#{ENV['FLICKR_KEY']}&user_id=#{params[:flickr_id]}&format=json&nojsoncallback=1")
+      @user_photos = JSON.parse(@response)
+     
+      @user_photos["photos"]["photo"].each do |photo| 
+  
+          Url.new(photo["server"],photo["id"], photo["secret"])
+          
+          end 
+    end
   
 
   end
@@ -32,8 +31,6 @@ class StaticPagesController < ApplicationController
 
   end 
 
-  #def set_user_photos
-   # @user_photos = {"photos"=> { "photo"=> [ {"id"=> "1", "owner"=>"me", "secret"=>"none", "server"=> "local"}]}}
-  #end
+
 
 end
