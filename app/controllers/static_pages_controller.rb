4216ee4f95d6f5733686413d9202da4074460c97
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-08-06 13:49:57 -0500

    Refactor controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,31 +1,13 @@
 class StaticPagesController < ApplicationController
 
-  def index
-
-    require 'rest-client'
-    require 'json'
-
-    url_base = 'https://www.flickr.com/services/rest/?'
-
-   
-  
-    
+  def index 
     if params[:flickr_id].present?
-      @response = RestClient.get( "#{url_base}method=flickr.people.getPublicPhotos&api_key=#{ENV['FLICKR_KEY']}&user_id=#{params[:flickr_id]}&format=json&nojsoncallback=1")
-      @user_photos = JSON.parse(@response)
-     
-      @user_photos["photos"]["photo"].each do |photo| 
-  
-          Url.new(photo["server"],photo["id"], photo["secret"])
-          
-          end 
+      API.get_data(params[:flickr_id])
     end
-  
-
   end
 
   private
-  
+
   def flickr_params
     params.permit(:flickr_id)
 
