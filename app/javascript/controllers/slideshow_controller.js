068b5b5c00eedc68b35f536eb9bcdca4e976ad37
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-07 20:08:03 -0500

    Add slideshow fucntionality

---
+++ app/javascript/controllers/slideshow_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "slide" ]
+  static values = { index: Number }
+
+  next() {
+    this.indexValue++
+  }
+
+  previous() {
+    this.indexValue--
+  }
+
+  indexValueChanged() {
+    this.showCurrentSlide()
+  }
+
+  showCurrentSlide() {
+    this.slideTargets.forEach((element, index) => {
+      element.hidden = index !== this.indexValue
+    })
+  }
+}
